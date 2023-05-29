df79f9d5993ce8f564f8b74481e792e32ab548e8
Kaushal Soni <kaushal.sce20@sot.pdpu.ac.in>
2023-05-29 11:05:19 +0530

    added the registration functionality and make the UI responsive for the pages wherever necessary

--- src/Pages/NewUser/NewUSER.jsx
+++ src/Pages/NewUser/NewUSER.jsx
@@ -1,30 +1,75 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
-import "../Login/Login.css";
 import axios from "axios";
-import firebase from "../../firebase";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const saveUserData = async (userData) => {
+  try {
+    const response = await axios.post(
+      "https://metrimonial.onrender.com/api/profile",
+      userData
+    );
+    // Handle the response if needed
+    console.log(response.data);
+  } catch (error) {
+    // Handle the error
+    console.error(error);
+  }
+};
 
 const NewUSER = () => {
-    let navigate = useNavigate();
-    const routeChange = () => { 
-        let path = `/Otp`; 
-        navigate(path);
-      }
+  const [user_name, setuser_name] = useState("");
+  const [contact_no, setcontact_no] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // let navigate = useNavigate();
+
+  const handleSubmit = () => {
+    const userData = {
+      user_name: user_name,
+      contact_no: contact_no,
+      email: email,
+      password: password,
+    };
+
+    saveUserData(userData);
+
+    // let path = "/Otp";
+    // navigate(path);
+  };
+
   return (
     <>
       <div className="login__wrapepr">
         <div className="login login-padding">
-            <h2>User Details</h2>
-          <input placeholder="First Name" type="text"/>
-          <input placeholder="Last Name" type="text"/>
-          <input placeholder="Mobile Number" type="tel"/>
-          <input placeholder="Email" type="email"/>
-          <input placeholder="Password" type="password"/>
-          <input placeholder="Confirm Password" type="password"/>
+          <h2>User Details</h2>
+          <input
+            placeholder="Username"
+            type="text"
+            onChange={(e) => setuser_name(e.target.value)}
+          />
+          <input
+            placeholder="Mobile Number"
+            type="tel"
+            name="contact_no"
+            id="contact_no"
+            onChange={(e) => setcontact_no(e.target.value)}
+          />
+          <input
+            placeholder="Email"
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            placeholder="Password"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input placeholder="Confirm Password" type="password" />
           <div id="recaptcha"></div>
-          <button onClick={routeChange} className="text-light">Next</button>
+          <button onClick={handleSubmit} className="text-light">
+            Next
+          </button>
         </div>
       </div>
     </>
