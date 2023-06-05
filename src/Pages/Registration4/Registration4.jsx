98304f14a91c0e9381e50606e22b7f7acf1af888
Kaushal Soni <kaushal.sce20@sot.pdpu.ac.in>
2023-06-05 17:33:12 +0530

    added the dropdown functionality of country and state from the country.json file

--- src/Pages/Registration4/Registration4.jsx
+++ src/Pages/Registration4/Registration4.jsx
@@ -1,11 +1,77 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./Registration4.css";
+import country from "../../country.json";
 
 function Registration4() {
+  const [maritalStatus, setMaritalStatus] = useState([]);
+  const [religion, setReligion] = useState([]);
+  const [motherTongues, setMotherTongues] = useState([]);
+  const [highestQualification, setHighestQualification] = useState([]);
+  const [salary, setSalary] = useState([]);
+  const [foodPreference, setFoodPreference] = useState([]);
+  const [drinkPreference, setDrinkPreference] = useState([]);
+  const [smokePreference, setSmokePreference] = useState([]);
+
+  useEffect(() => {
+    loadPreference();
+  }, []);
+
+  const Token = localStorage.getItem("token");
+
+  const loadPreference = async () => {
+    try {
+      const response = await fetch(
+        "https://metrimonial.onrender.com/api/preference/preferencedata",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: Token,
+          },
+        }
+      );
+      const responseData = await response.json();
+      console.log(responseData);
+      console.log(responseData.data);
+      setMaritalStatus(responseData.data.marital_status);
+      setReligion(responseData.data.religion);
+      setMotherTongues(responseData.data.mother_tongue);
+      setHighestQualification(responseData.data.highest_qualification);
+      setSalary(responseData.data.salary);
+      setFoodPreference(responseData.data.food_preference);
+      setDrinkPreference(responseData.data.drink);
+      setSmokePreference(responseData.data.smoke);
+    } catch (error) {
+      console.log("ERROR: ", error);
+    }
+  };
+
+  const [countryid, setCountryid] = useState("");
+  const [state, setState] = useState([]);
+  const [stateid, setStateid] = useState("");
+
+  const handlecounty = (e) => {
+    const getcountryId = e.target.value;
+    const getStatedata = country.find(
+      (country) => country.country_id === getcountryId
+    ).states;
+    setState(getStatedata);
+    setCountryid(getcountryId);
+    //console.log(getcountryId);
+  };
+
+  const handlestate = (e) => {
+    const stateid = e.target.value;
+    //console.log(stateid);
+    setStateid(stateid);
+  };
+
   return (
     <div>
-      <div className="login__wrapepr">
-        <div className="login login-padding">
+      <div className="p_login__wrapepr">
+        <div className="p_login p_login-padding">
           <h2 className="title_partnerpreference">Partner Preference</h2>
+
           <input
             placeholder="Age"
             type="text"
@@ -24,50 +90,90 @@ function Registration4() {
           <div className="gender_state">
             <select className="gender">
               <option value="">Marital Status</option>
-              <option value="Married">Married</option>
-              <option value="Un-Married">Un-Married</option>
+              {maritalStatus &&
+                maritalStatus.map((maritalStatus, index) => (
+                  <option key={index} value={maritalStatus.status}>
+                    {maritalStatus.status}
+                  </option>
+                ))}
             </select>
           </div>
           <div className="gender_state">
             <select className="gender">
               <option value="">Relegion</option>
-              <option value="Hindu">Hindu</option>
-              <option value="Muslim">Muslim</option>
+              {religion &&
+                religion.map((religion, index) => (
+                  <option key={index} value={religion.religious_name}>
+                    {religion.religious_name}
+                  </option>
+                ))}
             </select>
           </div>
           <div className="gender_state">
             <select className="gender">
-              <option value="">Mother's Tongue</option>
-              <option value="Hindi">Hindi</option>
-              <option value="Gujarati">Gujarati</option>
-            </select>
-          </div>
-
-          <input
-            placeholder="Highest Qualification"
-            type="text"
-            // onChange={(e) => setuser_name(e.target.value)}
-          />
-
-          <div className="gender_state">
-            <select className="gender">
-              <option value="">Annual Income</option>
-              <option value="0-2_LPA">0-2 LPA</option>
-              <option value="2-7_LPA">2-7 LPA</option>
+              <option value="">Mother Tongue</option>
+              {motherTongues &&
+                motherTongues.map((motherTongues, index) => (
+                  <option key={index} value={motherTongues.mt_name}>
+                    {motherTongues.mt_name}
+                  </option>
+                ))}
             </select>
           </div>
 
           <div className="gender_state">
             <select className="gender">
+              <option value="">Highest Qualification</option>
+              {highestQualification &&
+                highestQualification.map((highestQualification, index) => (
+                  <option key={index} value={highestQualification.hq_name}>
+                    {highestQualification.hq_name}
+                  </option>
+                ))}
+            </select>
+          </div>
+
+          <div className="gender_state">
+            <select className="gender">
+              <option value="">Salary</option>
+              {salary &&
+                salary.map((salary, index) => (
+                  <option key={index} value={salary.salary_value}>
+                    {salary.salary_value}
+                  </option>
+                ))}
+            </select>
+          </div>
+
+          <div className="gender_state">
+            <select className="gender" onChange={(e) => handlecounty(e)}>
               <option value="">Country</option>
-              <option value="India">India</option>
+              {country.map((country, index) => {
+                return (
+                  <option value={country.country_id} key={index}>
+                    {country.country_name}
+                  </option>
+                );
+              })}
               <option value="USA">USA</option>
             </select>
           </div>
 
           <div className="gender_state">
-            <select className="gender">
+            <select className="gender" onChange={(e) => handlestate}>
               <option value="">State</option>
+              {state.map((getstate, index) => (
+                <option value={getstate.state_id} key={index}>
+                  {getstate.state_name}
+                </option>
+              ))}
+              {/* <option value="Ahmedabad">Ahmedabad</option> */}
+            </select>
+          </div>
+
+          <div className="gender_state">
+            <select className="gender">
+              <option value="">City</option>
               <option value="Gujarat">Gujarat</option>
               <option value="Ahmedabad">Ahmedabad</option>
             </select>
@@ -76,25 +182,35 @@ function Registration4() {
           <div className="gender_state">
             <select className="gender">
               <option value="">Diet</option>
-              <option value="Vegeterian">Vegeterian</option>
-              <option value="Non-Vegeterian">Non-Vegeterian</option>
+              {highestQualification &&
+                foodPreference.map((foodPreference, index) => (
+                  <option key={index} value={foodPreference.fp_name}>
+                    {foodPreference.fp_name}
+                  </option>
+                ))}
             </select>
           </div>
 
           <div className="gender_state">
             <select className="gender">
               <option value="">Drink</option>
-              <option value="Ocassionally">Ocassionally</option>
-              <option value="Never">Never</option>
-              <option value="Sometimes">Sometimes</option>
+              {drinkPreference &&
+                drinkPreference.map((drinkPreference, index) => (
+                  <option key={index} value={drinkPreference.drink_value}>
+                    {drinkPreference.drink_value}
+                  </option>
+                ))}
             </select>
           </div>
           <div className="gender_state">
             <select className="gender">
               <option value="">Smoke</option>
-              <option value="Ocassionally">Ocassionally</option>
-              <option value="Never">Never</option>
-              <option value="Sometimes">Sometimes</option>
+              {smokePreference &&
+                smokePreference.map((smokePreference, index) => (
+                  <option key={index} value={smokePreference.smoke_value}>
+                    {smokePreference.smoke_value}
+                  </option>
+                ))}
             </select>
           </div>
           <div id="recaptcha"></div>
