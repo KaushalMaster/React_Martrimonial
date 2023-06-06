a38d9df544ece10e8967cd1ff572aed2646850be
Kaushal Soni <kaushal.sce20@sot.pdpu.ac.in>
2023-06-06 11:32:02 +0530

    updated the height feild in registration1 page from a static input to a dropdown input of height

--- src/Pages/Registration1/Registration1.jsx
+++ src/Pages/Registration1/Registration1.jsx
@@ -7,6 +7,57 @@ import "../Login/Login.css";
 import { useNavigate } from "react-router-dom";
 
 const Registration1 = () => {
+  const Height = [
+    "4’0”",
+    "4’1”",
+    "4’2”",
+    "4’3”",
+    "4’4”",
+    "4’5”",
+    "4’6”",
+    "4’7”",
+    "4’8”",
+    "4’9”",
+    "4’10”",
+    "4’11”",
+    "5’0”",
+    "5’1”",
+    "5’2”",
+    "5’3”",
+    "5’4”",
+    "5’5”",
+    "5’6”",
+    "5’7”",
+    "5’8”",
+    "5’9”",
+    "5’10”",
+    "5’11”",
+    "6’0”",
+    "6’1”",
+    "6’2”",
+    "6’3”",
+    "6’4”",
+    "6’5”",
+    "6’6”",
+    "6’7”",
+    "6’8”",
+    "6’9”",
+    "6’10”",
+    "6’11”",
+    "7’0”",
+    "7’1”",
+    "7’2”",
+    "7’3”",
+    "7’4”",
+    "7’5”",
+    "7’6”",
+    "7’7”",
+    "7’8”",
+    "7’9”",
+    "7’10”",
+    "7’11”",
+  ];
+
   let navigate = useNavigate();
   const routeChange = () => {
     let path = `/Profile`;
@@ -200,7 +251,16 @@ const Registration1 = () => {
             </select>
           </div>
           <input placeholder="DOB" type="Date" className="dob" />
-          <input placeholder="Height" type="text" />
+          <div className="gender_state">
+            <select className="gender">
+              <option value="">Height</option>
+              {Height.map((height, index) => (
+                <option key={index} value={height}>
+                  {height}
+                </option>
+              ))}
+            </select>
+          </div>
           <input placeholder="Weight" type="text" />
           <input placeholder="City" type="text" />
           <div className="gender_state">
